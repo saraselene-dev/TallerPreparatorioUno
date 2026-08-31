fbed6fc40b4c3e6f73223c04588bd8eb35f3f521
saraselene-dev <saraselene@gmail.com>
2026-08-31 05:39:02 -0500

    Clases: objeto, métodos y principal.

---
+++ ObjProducto10.java
@@ -0,0 +1,23 @@
+public class ObjProducto10 {
+    private String Nombre;
+    private boolean EnOferta;
+    public ObjProducto10() {
+    }
+    public ObjProducto10(String nombre, boolean enOferta) {
+        Nombre = nombre;
+        EnOferta = enOferta;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    public boolean isEnOferta() {
+        return EnOferta;
+    }
+    public void setEnOferta(boolean enOferta) {
+        EnOferta = enOferta;
+    }
+
+}
