da54d622825782fc8a4e701383e8f0eb901e4e05
saraselene-dev <saraselene@gmail.com>
2026-08-31 03:34:23 -0500

    Objeto, métodos y menú.

---
+++ ObjEstudiante7.java
@@ -0,0 +1,31 @@
+public class ObjEstudiante7{
+    private String Nombre;
+    private String Nota;
+
+    public ObjEstudiante7() {
+    }
+
+    public ObjEstudiante7(String nombre, String nota) {
+        Nombre = nombre;
+        Nota = nota;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getNota() {
+        return Nota;
+    }
+
+    public void setNota(String nota) {
+        Nota = nota;
+    }
+    
+
+    
+}
